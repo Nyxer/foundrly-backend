7c3751174d479ebb141a971b7ab6e56fc8f522c3
Nyxer <98869782+Nyxer@users.noreply.github.com>
2025-05-12 19:30:22 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const generateCode = require("./routes/generateCode");
 
 dotenv.config();
 const app = express();
+const cors = require("cors");
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/refine-idea", refineIdea);
